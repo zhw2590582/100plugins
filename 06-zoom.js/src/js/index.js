7f952c0ab1cfd33241165f4c362d95d527bc5f94
Harvey Zack <laozhaochaguan@hotmail.com>
2017-05-02 12:56:01 +0800

    update

--- 06-zoom.js/src/js/index.js
+++ 06-zoom.js/src/js/index.js
@@ -13,6 +13,7 @@ class Zoom {
 
   _eventBind(){
     this.elements.forEach(item => {
+      item.className += ' zoom_item';
       item.addEventListener('click', this._imgClick, false);
     })
   }
@@ -20,33 +21,41 @@ class Zoom {
   _imgClick(e){
     this.imgTarget = e.target;
     if (document.getElementById('zoom_modal') === null) {
-      this._createElement(e);
+      this._createElement();
     } else {
       this.imgTarget.style.visibility = 'hidden';
       this.zoomModal.style.display = 'flex';
-      this._insertElement(e);
+      this._insertElement();
     }
 
   }
 
-  _createElement(e){
+  _createElement(){
     this.zoomModal = document.createElement('div');
     this.zoomModal.setAttribute('id', 'zoom_modal');
     this.zoomModal.addEventListener('click', this._modalClose, false);
     this.zoomImg = document.createElement('img');
-    this.zoomImg.src = e.target.getAttribute('data-zoom');
+    this.zoomImg.src = this.imgTarget.getAttribute('data-zoom');
     this.zoomImg.setAttribute('id', 'zoom_img');
     this.zoomModal.appendChild(this.zoomImg);
     document.body.appendChild(this.zoomModal);
   }
 
-  _insertElement(e){
-    this.zoomImg.src = e.target.getAttribute('data-zoom');
+  _insertElement(){
+    this.zoomImg.src =this.imgTarget.getAttribute('data-zoom');
   }
 
   _modalClose(){
     this.zoomModal.style.display = 'none';
     this.imgTarget.style.visibility = 'visible';
+  }
+
+  _getStyle(el, attr) {
+    if (el && el.currentStyle) {
+      return el.currentStyle[attr]
+    } else {
+      return window.getComputedStyle(el)[attr]
+    }
   }
 
 }
