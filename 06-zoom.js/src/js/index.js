39190090bcbeae25e26d4ce41ae21660e4a58b94
Harvey Zack <laozhaochaguan@hotmail.com>
2017-05-02 13:41:31 +0800

    update

--- 06-zoom.js/src/js/index.js
+++ 06-zoom.js/src/js/index.js
@@ -23,31 +23,71 @@ class Zoom {
     if (document.getElementById('zoom_modal') === null) {
       this._createElement();
     } else {
-      this.imgTarget.style.visibility = 'hidden';
-      this.zoomModal.style.display = 'flex';
-      this._insertElement();
+      this._createElement(true);
     }
-
   }
 
-  _createElement(){
+  _createElement(type){
+    if (type) {
+      this.zoomImg.src = this.imgTarget.getAttribute('data-zoom');
+      this.zoomAnimate.src = this.imgTarget.src;
+      this._showAnimate();
+      return;
+    }
     this.zoomModal = document.createElement('div');
     this.zoomModal.setAttribute('id', 'zoom_modal');
     this.zoomModal.addEventListener('click', this._modalClose, false);
+
     this.zoomImg = document.createElement('img');
     this.zoomImg.src = this.imgTarget.getAttribute('data-zoom');
     this.zoomImg.setAttribute('id', 'zoom_img');
     this.zoomModal.appendChild(this.zoomImg);
+
+    this.zoomAnimate = document.createElement('img');
+    this.zoomAnimate.src = this.imgTarget.src;
+    this.zoomAnimate.setAttribute('id', 'zoom_animate');
+    this.zoomModal.appendChild(this.zoomAnimate);
+
     document.body.appendChild(this.zoomModal);
+    this._showAnimate();
   }
 
-  _insertElement(){
-    this.zoomImg.src =this.imgTarget.getAttribute('data-zoom');
+  _showAnimate(){
+
+
+    let imgTargetWidth = this._getStyle(this.imgTarget, 'width').replace(/px/, '') - 0;
+    let imgTargetHeight = this._getStyle(this.imgTarget, 'height').replace(/px/, '') - 0;
+    let imgTargetTop = this.imgTarget.getBoundingClientRect().top;
+    let imgTargetLeft = this.imgTarget.getBoundingClientRect().left;
+
+    this.zoomAnimate.style.width = imgTargetWidth + 'px';
+    this.zoomAnimate.style.height = imgTargetHeight + 'px';
+    this.zoomAnimate.style.top = imgTargetTop + 'px';
+    this.zoomAnimate.style.left = imgTargetLeft + 'px';
+
+    this.imgTarget.style.visibility = 'hidden';
+    this.zoomModal.style.display = 'flex';
+
+    let self = this;
+    setTimeout(function () {
+      self.zoomAnimate.style.width = 1000 + 'px';
+      self.zoomAnimate.style.height = 667 + 'px';
+      self.zoomAnimate.style.top = imgTargetTop + 100 + 'px';
+      self.zoomAnimate.style.left = imgTargetLeft + 100 + 'px';
+    }, 0)
+
+    setTimeout(function () {
+      self.zoomImg.style.visibility = 'visible';
+      self.zoomAnimate.style.visibility = 'hidden';
+    }, 500)
+
   }
 
   _modalClose(){
     this.zoomModal.style.display = 'none';
     this.imgTarget.style.visibility = 'visible';
+    this.zoomImg.style.visibility = 'hidden';
+    this.zoomAnimate.style.visibility = 'visible';
   }
 
   _getStyle(el, attr) {
