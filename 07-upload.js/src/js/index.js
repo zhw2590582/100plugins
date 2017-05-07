5e7ee69438f5df422404177090f9c0a6962aa120
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-05-07 21:28:32 +0800

    update

---
+++ 07-upload.js/src/js/index.js
@@ -0,0 +1,33 @@
+class Upload {
+  constructor(options) {
+
+    this._init();
+  }
+
+  _init() {
+    this._XHR()
+  }
+
+  _XHR(){
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials=true;
+    xhr.open("POST", "http://jsonplaceholder.typicode.com/posts");
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xhr.send("title=foo&body=bar&userId=1");
+    xhr.upload.onprogress = function(event) {
+        if (event.lengthComputable) {
+            var percentComplete = (event.loaded / event.total) * 100;
+            console.log(0);
+        }
+    }
+    xhr.onload = function(){
+        if(this.status === 201){
+          console.log(this);
+        }
+    }
+  }
+
+}
+
+window.Upload = Upload
+export default window.Upload
