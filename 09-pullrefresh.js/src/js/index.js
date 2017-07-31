fe99e936552cf8f4fe1e26e0e32b7ad70de80902
Harvey Zack <laozhaochaguan@hotmail.com>
2017-07-31 12:23:00 +0800

    update

---
+++ 09-pullrefresh.js/src/js/index.js
@@ -0,0 +1,10 @@
+import listen from 'good-listener';
+
+class Pullrefresh {
+  constructor(options) {
+    console.log(this);
+  }
+}
+
+window.Pullrefresh = Pullrefresh;
+module.exports = Pullrefresh;
