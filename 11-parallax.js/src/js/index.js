94be2f17143370ca8792382102230f2423542704
Harvey Zack <laozhaochaguan@hotmail.com>
2017-08-04 16:03:43 +0800

    update

--- 11-parallax.js/src/js/index.js
+++ 11-parallax.js/src/js/index.js
@@ -8,10 +8,13 @@ class Parallax {
     };
 
     this.config = {
-      state: false,
+      pause: false,
+      posY: 0,
+      transformProp: this._transformProp(),
       screenHeight: window.innerHeight || 0,
       elems: [],
-      elemsInit: []
+      elemsInit: [],
+      listeners: []
     };
 
     this.options.speed = this._clamp(this.options.speed, -10, 10);
@@ -20,8 +23,12 @@ class Parallax {
     if (elements.length > 0) {
       this.config.elems = [].slice.call(elements);
     } else {
-      throw new Error(`The elements(${el}) you're trying to select don't exist.`);
+      throw new Error(
+        `The elements(${el}) you're trying to select don't exist.`
+      );
     }
+
+    this._animate = this._animate.bind(this);
 
     this._init();
   }
@@ -34,11 +41,64 @@ class Parallax {
 
   _init() {
     this._elemsInit();
+    this._eventBind();
     console.log(this);
   }
 
-  _elemsInit(){
-    
+  _elemsInit() {
+    for (var i = 0; i < this.config.elems.length; i++) {
+      var initInfo = this._createInit(this.config.elems[i]);
+      this.config.elemsInit.push(initInfo);
+    }
+  }
+
+  _createInit(el) {
+    return {
+      el: el,
+      speed: +el.getAttribute('data-parallax-speed') || this.options.speed,
+      top: el.getBoundingClientRect().top,
+      height: el.clientHeight || el.offsetHeight || el.scrollHeight || 0
+    };
+  }
+
+  _eventBind(){
+    this._setPosition();
+    window.addEventListener('resize', this._animate);
+    this._update();
+    this._animate();
+  }
+
+  _setPosition() {
+    let oldY = this.config.posY;
+
+    if (window.pageYOffset !== undefined) {
+      this.config.posY = window.pageYOffset;
+    } else {
+      this.config.posY = (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    }
+
+    if (oldY != this.config.posY) {
+      return true;
+    }
+
+    return false;
+  }
+
+  _animate(e){
+    for (var i = 0; i < this.config.elems.length; i++){
+
+    }
+  }
+
+  _update(){
+    let time = 0;
+    time++;
+    console.log(time);
+    if (this._setPosition() && this.config.pause === false) {
+      this._animate();
+    }
+
+    this._loop(this._update);
   }
 
   /**
@@ -79,6 +139,11 @@ class Parallax {
   _clamp(num, min, max) {
     return num <= min ? min : num >= max ? max : num;
   }
+
+  _listen(el, type, event) {
+    this.config.listeners.push(listen(el, type, event));
+  }
+
 }
 
 window.Parallax = Parallax;
