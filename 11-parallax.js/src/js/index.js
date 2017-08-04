6268ebfe298c0a180bf4d08be6b55103148e4be6
Harvey Zack <laozhaochaguan@hotmail.com>
2017-08-04 12:27:34 +0800

    update

--- 11-parallax.js/src/js/index.js
+++ 11-parallax.js/src/js/index.js
@@ -1,32 +1,84 @@
 import listen from 'good-listener';
 
 class Parallax {
-  constructor(options) {
-
+  constructor(el = '.parallax', options) {
     this.options = {
-      ...Validate.DEFAULTS,
+      ...Parallax.DEFAULTS,
       ...options
     };
 
     this.config = {
+      state: false,
+      screenHeight: window.innerHeight || 0,
+      elems: [],
+      elemsInit: []
+    };
 
+    this.options.speed = this._clamp(this.options.speed, -10, 10);
+
+    let elements = document.querySelectorAll(el);
+    if (elements.length > 0) {
+      this.config.elems = [].slice.call(elements);
+    } else {
+      throw new Error(`The elements(${el}) you're trying to select don't exist.`);
     }
 
-
     this._init();
-
   }
 
   static get DEFAULTS() {
     return {
-
+      speed: -2
     };
   }
 
-  _init(){
-
+  _init() {
+    this._elemsInit();
+    console.log(this);
   }
 
+  _elemsInit(){
+    
+  }
+
+  /**
+  * ================================== HELPER ==================================
+  */
+
+  _loop() {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      function(callback) {
+        setTimeout(callback, 1000 / 60);
+      }
+    );
+  }
+
+  _transformProp() {
+    return (
+      window.transformProp ||
+      (function() {
+        var testEl = document.createElement('div');
+        if (testEl.style.transform == null) {
+          var vendors = ['Webkit', 'Moz', 'ms'];
+          for (var vendor in vendors) {
+            if (testEl.style[vendors[vendor] + 'Transform'] !== undefined) {
+              return vendors[vendor] + 'Transform';
+            }
+          }
+        }
+        return 'transform';
+      })()
+    );
+  }
+
+  _clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num;
+  }
 }
 
 window.Parallax = Parallax;
