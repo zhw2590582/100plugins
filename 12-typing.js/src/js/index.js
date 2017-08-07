67e6c1581091d08b8fb9a0b1054d19296492d8a5
Harvey Zack <laozhaochaguan@hotmail.com>
2017-08-07 16:36:16 +0800

    update

--- 12-typing.js/src/js/index.js
+++ 12-typing.js/src/js/index.js
@@ -9,19 +9,6 @@ class Typing {
 
     };
 
-    window.requestAnimFrame = (function() {
-      return (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function(callback) {
-          window.setTimeout(callback, 1000 / 60);
-        }
-      );
-    })();
-
     this._init();
   }
 
