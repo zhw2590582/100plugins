cc54445199d31487cb1f28544909c26a312b627f
Harvey Zack <laozhaochaguan@gmail.com>
2018-01-04 17:46:07 +0800

    uodate

--- 21-scrolldir.js/src/index.js
+++ 21-scrolldir.js/src/index.js
@@ -1,9 +1,47 @@
-import './index.scss';
+let win = window;
+let el = document.documentElement;
+let body = document.body;
+let e;
+
+let config = {
+  off: false,
+  attribute: 'data-scrolldir',
+  el: '',
+  direction: 'down'
+};
+
+let tick = () => {
+  console.log(e);
+};
+
+let handler = event => {
+  e = event;
+  return win.requestAnimationFrame(tick);
+};
 
 let scrolldir = options => {
-  Promise.resolve().then(() => {
-    console.log('hi');
-  })
+  Object.assign(config, options);
+
+  if (config.el && typeof config.el === 'string') {
+    el = document.querySelector(config.el);
+    if (!el) throw TypeError('el is not an element');
+  }
+
+  if (config.off === true) {
+    el.setAttribute(config.attribute, 'off');
+    return win.removeEventListener('scroll', handler);
+  }
+
+  if (
+    config.attribute &&
+    typeof config.attribute === 'string' &&
+    config.direction &&
+    typeof config.direction === 'string'
+  ) {
+    el.setAttribute(config.attribute, config.direction);
+  }
+
+  return win.addEventListener('scroll', handler);
 };
 
 window.scrolldir = scrolldir;
