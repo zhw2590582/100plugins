28dce54bb2022638533da4029a2892b226444777
zhaohaiwei <zhaohaiwei@xinghehudong.com>
2018-03-21 16:41:23 +0800

    update

--- 21-lottery.js/README.md
+++ 21-lottery.js/README.md
@@ -5,9 +5,30 @@
 ## 使用
 
 ```js
-import Lottery from './js/lottery.js';
+import Lottery from "./js/lottery.js";
 
-var app = new Lottery();
+// 第一个参数为容器，第二个参数为配置对象
+var app = new Lottery(".lottery", {
+  items: ".item", // 奖品类名
+  itemsLength: 10, // 奖品个数，必须与实际奖品类名的个数一致
+  time: 10000, // 运行时间（与实际时间不一致，有待优化）
+  begin: function() { // 开始钩子
+    console.log("抽奖开始");
+  },
+  end: function(index) { // 结束钩子，参数为中奖奖品的下标
+    alert("中奖号码下标为：" + index);
+  }
+});
+
+// 设置中奖下标
+app.prize(7);
+
+// 开始抽奖
+app.start();
+
+// 是否禁用抽奖，传一个布尔值
+app.disabled(true)
+
 ```
 
 ## 开发
--- 21-lottery.js/src/index.js
+++ 21-lottery.js/src/index.js
@@ -1,35 +1,157 @@
 import './index.scss';
+import dom from './dom.js';
 
 class Lottery {
-  constructor(el, options) {
+  constructor(el = '.lottery', options) {
     this.options = {
       ...Lottery.DEFAULTS,
       ...options
     };
+
+    this.config = {
+      containerEl: el instanceof Element ? el : document.querySelector(el),
+      items: [],
+      current: -1,
+      prize: null,
+      disabled: false,
+      beginTime: 0,
+      currentSpeed: 200,
+    }
 
     this._init();
   }
 
   static get DEFAULTS() {
     return {
-
+      items: '.item',
+      itemsLength: 10,
+      time: 5000,
+      begin: new Function, 
+      end: new Function
     };
   }
 
   _init(){
-    console.log(this);
+    dom.addClass(this.config.containerEl, '__lottery__');
+    this.config.items = dom.find(this.config.containerEl, this.options.items);
+    if(this.config.items.length !== this.options.itemsLength){
+      throw new TypeError(`items dom length(${this.config.items.length}) is not equal to options itemsLength(${this.options.itemsLength})`);
+    }
+    if(this.options.time < 5000){
+      throw new TypeError(`options time(${this.options.time}) can\'t be less than 5000`);
+    }
+  }
+
+  _begin(){
+    this.options.begin();
+    this.config.beginTime = new Date().getTime();
+    dom.addClass(this.config.containerEl, '__lottery__processing');
+    return this;
+  }
+
+  _animate(step){
+    this._itemClass(++this.config.current);
+
+    // 时间差
+    let taketime = new Date().getTime() - this.config.beginTime;
+
+    // 两秒加速
+    if(taketime < 2000){
+      this.config.currentSpeed -= 6;
+    }
+
+    // 匀速
+    if(taketime >= 2000 && taketime <= this.options.time - 3000){
+      this.config.currentSpeed = 100;
+    }
+
+    // 两秒减速
+    if(taketime >= this.options.time - 3000 && taketime < this.options.time){
+      this.config.currentSpeed += 6;
+    }
+
+    // 结束
+    if(taketime >= this.options.time){
+      this._location();
+    } else {
+      setTimeout(() => {
+        this._animate();
+      }, this.config.currentSpeed);
+    }
+  }
+
+  _location(){
+    this.config.spaceStep = this.config.prize - this.config.current;
+    if(this.config.spaceStep === 0){
+      this._end();
+    } else if(this.config.spaceStep > 0){
+      this._animateStep();
+    } else {
+      this.config.spaceStep = this.options.itemsLength - this.config.current + this.config.prize;
+      this._animateStep();
+    }
+  }
+
+  _animateStep(){
+    this._itemClass(++this.config.current);
+    this.config.spaceStep -= 1;
+    setTimeout(() => {
+      if(this.config.spaceStep === 0){
+        this._end();
+      } else {
+        this._animateStep();
+      }
+    }, this.config.currentSpeed);
+  }
+
+  _itemClass(index){
+    this.config.items.forEach(item => dom.removeClass(item, '__lottery__active'));
+    dom.addClass(this.config.items[index], '__lottery__active');
+    if(this.config.current === this.options.itemsLength - 1){
+      this.config.current = -1;
+    }
+  }
+
+  _end(){
+    this.config.beginTime = 0;
+    this.config.current = -1;
+    dom.removeClass(this.config.containerEl, '__lottery__processing');
+    this.disabled(false);
+    this.options.end(this.config.prize);
+    return this;
   }
 
   prize(index){
-    console.log(index);
+    if(typeof index !== 'number'){
+      throw new TypeError(`Expected number type, but get ${typeof index}`);
+    }
+
+    if(index < 0 || index > this.options.itemsLength - 1){
+      throw new TypeError(`Expected number greater than 0 and Less than ${this.options.itemsLength - 1};`);
+    }
+    if(this.config.disabled) return this;
+    this.config.prize = index;
     return this;
   }
 
   start(){
+    if(!this.config.prize){
+      throw new TypeError(`Please pass prize function a numeric parameter first`);
+    }
+    if(this.config.disabled) return this;
+    this._begin();
+    this.disabled(true);
+    this._animate();
     return this;
   }
 
-  disabled(){
+  disabled(type){
+    this.config.disabled = type;
+    if(type){
+      dom.addClass(this.config.containerEl, '__lottery__disabled');
+    } else {
+      dom.removeClass(this.config.containerEl, '__lottery__disabled');
+    }
     return this;
   }
 }
