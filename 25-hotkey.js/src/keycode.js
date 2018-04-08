e90e371ac0f5df4421422ef21fe0d336fa3f578f
Harvey Zack <laozhaochaguan@gmail.com>
2018-04-08 21:02:47 +0800

    Update keycode.js

--- 25-hotkey.js/src/keycode.js
+++ 25-hotkey.js/src/keycode.js
@@ -73,8 +73,8 @@ for (i = 97; i < 123; i++) keycode[String.fromCharCode(i)] = i - 32;
 for (var i = 48; i < 58; i++) keycode[i - 48] = i;
 for (i = 1; i < 13; i++) keycode['f' + i] = i + 111;
 for (i = 0; i < 10; i++) keycode['numpad ' + i] = i + 96;
-for (i in keycode) keyrev[keycode[i]] = i;
 for (var alias in aliases) keycode[alias] = aliases[alias];
+for (i in keycode) keyrev[keycode[i]] = i;
 
 module.exports = {
   keycode,
