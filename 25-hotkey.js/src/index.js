2aaddda4cf8b2f373280e8831455da1e030eabc5
zhw2590582 <laozhaochaguan@gmail.com>
2018-04-09 18:30:23 +0800

    add test

--- 25-hotkey.js/src/index.js
+++ 25-hotkey.js/src/index.js
@@ -4,12 +4,21 @@ import { keycode, keyrev } from './keycode';
 let emitter = mitt();
 
 let hotkey = (key, callback) => {
-  key = key.replace(/\s/g, '');
+  if (typeof key !== 'string') {
+    throw new TypeError('key required String type');
+  } else if (typeof callback !== 'function') {
+    throw new TypeError('callback required function type');
+  }
+
+  key = key.replace(/\s/g, '').toLowerCase();
   emitter.on(key, callback);
 };
 
-let dispatch = e => {
-  console.log('dispatch');
+let dispatch = event => {
+  let key = event.keyCode;
+  let name = keyrev[key];
+  if (!name) throw new TypeError(`Unknown key: ${key}`);
+  emitter.emit(name, { event, key, name });
 };
 
 window.addEventListener('keydown', dispatch);
