ad6772bf8db1f1cc9a758012fd0a94931c21ae81
zhw2590582 <laozhaochaguan@gmail.com>
2018-04-10 16:04:49 +0800

    add test

--- 26-chosen.js/src/index.js
+++ 26-chosen.js/src/index.js
@@ -1,24 +1,80 @@
 import './index.scss';
-import dom from './dom.js';
+import dom from './dom';
+import error from './error';
 
 class Chosen {
-  constructor(options) {
+  constructor(el, options) {
     this.options = {
       ...Chosen.DEFAULTS,
       ...options
     };
 
+    this.listeners = [];
+    this.selected = [];
+    this._selectClick = this._selectClick.bind(this);
+    this._optionClick = this._optionClick.bind(this);
+    this._searchChange = this._searchChange.bind(this);
+    this._selectedDelClick = this._selectedDelClick.bind(this);
+    this._deselectClick = this._deselectClick.bind(this);
     this._init();
   }
 
   static get DEFAULTS() {
     return {
-
+      search: true,
+      no_results: "No Results!",
+      max_selected: null,
+      deselect: false
     };
   }
 
   _init() {
-    console.log(this)
+    this._creatDom();
+    this._eventBind();
+    console.log(this);
+  }
+
+  _creatDom() {
+
+  }
+
+  _eventBind() {
+    
+  }
+
+  _selectClick(){
+
+  }
+
+  _optionClick(){
+
+  }
+
+  _searchChange(){
+
+  }
+
+  _selectedDelClick(){
+
+  }
+
+  _deselectClick(){
+
+  }
+
+  _triggerChange(){
+    this.listeners.forEach(cb => cb(this.selected));
+    return this;
+  }
+
+  change(callback) {
+    error(typeof callback !== 'function', 'The change function parameter required function type, but get ' + typeof callback);
+    this.listeners.push(callback);
+    return this;
+  }
+
+  destroy(){
+
   }
 }
 
