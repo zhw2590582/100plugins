e852e3b0c86c317e74d7368f3f51ccc94dd8e762
zhw2590582 <laozhaochaguan@gmail.com>
2018-04-28 10:42:33 +0800

    update

--- 29-split.js/src/index.ts
+++ 29-split.js/src/index.ts
@@ -5,27 +5,26 @@ interface Options {
   onDragEnd?(): void;
 }
 
-interface Configs {
+interface Config {
   parentEl: HTMLElement;
-  childrenConfigs: ChildrenConfigs[];
+  childrenConfig: ChildrenConfig[];
   split: string;
   step: number;
   minSize: number;
   maxSize: number;
   defaultSize: number;
   resizerEl?: HTMLElement;
-  cacheStyle?: string;
+  cacheStyle: string | null;
   move: boolean;
-  cachePos?: {
+  cachePos: {
     x: number;
     y: number;
   };
 }
 
-interface ChildrenConfigs {
-  children: HTMLElement;
-  subConfigs?: Configs | null;
-  cacheStyle?: string;
+interface ChildrenConfig {
+  childrenEl: Element;
+  cacheStyle: string;
 }
 
 import './index.scss';
@@ -34,7 +33,7 @@ import { setStyles, hasClass } from './utils';
 class Split {
   private options: Options;
   private parentEl: HTMLElement;
-  private configs: Configs;
+  private configs: Config[];
 
   private constructor(options: Options = {}) {
     this.options = {
@@ -43,7 +42,7 @@ class Split {
     };
 
     this.parentEl = document.querySelector(options.parent);
-    this.configs = this._getConfigs(this.parentEl);
+    this.configs = [];
     this._mousedown = this._mousedown.bind(this);
     this._mousemove = this._mousemove.bind(this);
     this._mouseup = this._mouseup.bind(this);
@@ -63,13 +62,16 @@ class Split {
     if (!this.parentEl) {
       throw new TypeError(`Can't find dom element: options.parent`);
     }
-    this._styleInit(this.configs);
-    this._creatResizer(this.configs);
-    this._eventBind(this.configs);
+    this._getConfigs(this.parentEl);
+    this.configs.forEach(config => {
+      this._styleInit(config);
+      this._creatResizer(config);
+      this._eventBind(config);
+    });
     console.log(this);
   }
 
-  private _getConfigs(parentEl: HTMLElement): Configs {
+  private _getConfigs(parentEl: HTMLElement): void {
     let children = Array.from(parentEl.children);
     if (children.length > 2) {
       children = children.slice(0, 2);
@@ -79,19 +81,15 @@ class Split {
       hasClass(el, this.options.children.slice(1))
     );
     let defaultSize = Number(parentEl.dataset.defaultSize || 100);
-    let split = ['vertical', 'horizontal'].includes(parentEl.dataset.split) ? parentEl.dataset.split : 'vertical';
-    return {
+    let split = ['vertical', 'horizontal'].includes(parentEl.dataset.split)
+      ? parentEl.dataset.split
+      : 'vertical';
+    this.configs.push({
       parentEl: parentEl,
-      childrenConfigs: childrenEl.map((children: HTMLElement) => {
-        let subChildren = Array.from(children.children);
-        let subParentEl = subChildren.find(el =>
-          hasClass(el, this.options.parent.slice(1))
-        );
+      childrenConfig: childrenEl.map(el => {
         return {
-          children: children,
-          subConfigs: subParentEl
-            ? this._getConfigs(<HTMLElement>subParentEl)
-            : null
+          childrenEl: el,
+          cacheStyle: el.getAttribute('style')
         };
       }),
       minSize: Number(parentEl.dataset.minSize || 0),
@@ -99,15 +97,24 @@ class Split {
       defaultSize: defaultSize,
       split: split,
       step: Number(parentEl.dataset.step || 0),
+      cacheStyle: parentEl.getAttribute('style'),
       move: false,
       cachePos: {
         x: parentEl.dataset.split === 'vertical' ? defaultSize : 0,
         y: parentEl.dataset.split === 'horizontal' ? defaultSize : 0
       }
-    };
+    });
+
+    // 递归并展平
+    children.forEach(el => {
+      let subParent = Array.from(el.children).find(subEl =>
+        hasClass(subEl, this.options.parent.slice(1))
+      );
+      subParent && this._getConfigs(<HTMLElement>subParent);
+    });
   }
 
-  private _styleInit(configs: Configs): void {
+  private _styleInit(configs: Config): void {
     configs.cacheStyle = configs.parentEl.getAttribute('style');
     setStyles(
       configs.parentEl,
@@ -140,11 +147,10 @@ class Split {
           }
     );
 
-    if (!configs.childrenConfigs) return;
-    configs.childrenConfigs.forEach((item, index) => {
-      item.cacheStyle = item.children.getAttribute('style');
-      let isLast = index === configs.childrenConfigs.length - 1;
-      setStyles(item.children, {
+    if (!configs.childrenConfig) return;
+    configs.childrenConfig.forEach((item, index) => {
+      let isLast = index === configs.childrenConfig.length - 1;
+      setStyles(item.childrenEl, {
         flex: isLast ? '1 1 0%' : '0 0 auto',
         position: 'relative',
         outline: 'none',
@@ -152,31 +158,25 @@ class Split {
           ? 'auto'
           : configs.defaultSize + 'px'
       });
-      item.subConfigs && this._styleInit(item.subConfigs);
     });
   }
 
-  private _creatResizer(configs: Configs): void {
-    if (!configs.childrenConfigs) return;
-    configs.childrenConfigs.forEach((item, index) => {
-      let isLast = index === configs.childrenConfigs.length - 1;
+  private _creatResizer(configs: Config): void {
+    if (!configs.childrenConfig) return;
+    configs.childrenConfig.forEach((item, index) => {
+      let isLast = index === configs.childrenConfig.length - 1;
       if (!isLast) {
         let resizer = (configs.resizerEl = document.createElement('div'));
         resizer.setAttribute('class', `Resizer ${configs.split}`);
-        this._after(item.children, resizer);
+        this._after(item.childrenEl, resizer);
       }
-      item.subConfigs && this._creatResizer(item.subConfigs);
     });
   }
 
-  private _eventBind(configs: Configs): void {
+  private _eventBind(configs: Config): void {
     configs.resizerEl.addEventListener('mousedown', this._mousedown);
     configs.resizerEl.addEventListener('mousemove', this._mousemove);
     configs.resizerEl.addEventListener('mouseup', this._mouseup);
-    if (!configs.childrenConfigs) return;
-    configs.childrenConfigs.forEach(item => {
-      item.subConfigs && this._eventBind(item.subConfigs);
-    });
   }
 
   private _mousedown(e: MouseEvent): void {
@@ -199,15 +199,17 @@ class Split {
     }
   }
 
-  public destroy(configs: Configs): void {
-    configs.resizerEl.removeEventListener('mousedown', this._mousedown);
-    configs.resizerEl.removeEventListener('mousemove', this._mousemove);
-    configs.resizerEl.removeEventListener('mouseup', this._mouseup);
-    setStyles(configs.parentEl, configs.cacheStyle);
-    if (!configs.childrenConfigs) return;
-    configs.childrenConfigs.forEach(item => {
-      setStyles(item.children, item.cacheStyle);
-      item.subConfigs && this.destroy(item.subConfigs);
+  public destroy(): void {
+    this.configs.forEach(config => {
+      config.resizerEl.removeEventListener('mousedown', this._mousedown);
+      config.resizerEl.removeEventListener('mousemove', this._mousemove);
+      config.resizerEl.removeEventListener('mouseup', this._mouseup);
+      config.parentEl.style.cssText = config.cacheStyle;
+      if (!config.childrenConfig) return;
+      config.childrenConfig.forEach(item => {
+        let childrenEl = <HTMLElement>item.childrenEl;
+        childrenEl.style.cssText = item.cacheStyle;
+      });
     });
   }
 }
