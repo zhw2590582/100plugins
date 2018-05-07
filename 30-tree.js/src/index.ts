60688fc5c5dd8b2041c3cdf28c533c2d239371c6
zhw2590582 <laozhaochaguan@gmail.com>
2018-05-07 15:48:36 +0800

    add code

--- 30-tree.js/src/index.ts
+++ 30-tree.js/src/index.ts
@@ -1,14 +1,31 @@
 interface Options {
-
+  el: string;
+  nodes?: Tree[];
+  onCheck?: (e: MouseEvent, tree: Tree) => void;
+  onExpand?: (e: MouseEvent, tree: Tree) => void;
 }
+
+interface Tree {
+  value: string;
+  label: string;
+  checked?: boolean;
+  expanded?: boolean;
+  children?: Tree[];
+}
+
+interface Configs extends Tree {
+  target: HTMLElement;
+};
+
 
 import './index.scss';
 import {} from './utils';
 
 class Tree {
   private options: Options;
+  private configs: Configs;
 
-  private constructor(options: Options = {}) {
+  private constructor(options: Options) {
     this.options = {
       ...Tree.DEFAULTS,
       ...options
@@ -17,18 +34,37 @@ class Tree {
     this._init();
   }
 
-  static get DEFAULTS() {
+  static get DEFAULTS(): Options {
     return {
-
+      el: '',
+      nodes: [],
+      onCheck: function() {},
+      onExpand: function() {}
     };
   }
 
-  private _init() {
+  private _init(): void {
+    this._creatDom();
+  }
+
+  private _getConfigs(): void {
+    // 
+  }
+
+  private _creatDom(): void {
     console.log(this)
   }
 
-  public destroy() {
+  private _eventBind(): void {
+    //
+  }
 
+  public getTree(): void {
+    //
+  }
+
+  public destroy(): void {
+    //
   }
 }
 
